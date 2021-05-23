4975096d30f49c019940d432362862fa95cad9d9
26seung <yousi7@naver.com>
2021-05-23 16:28:29 +0900

    ..

--- slipp_spring/src/main/java/net/slipp/web/users/UserController.java
+++ slipp_spring/src/main/java/net/slipp/web/users/UserController.java
@@ -2,6 +2,7 @@ package net.slipp.web.users;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -16,26 +17,57 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import net.slipp.domain.users.User;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
-	@RequestMapping("/users/form")
+	@RequestMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("user", new User());
 		return "users/form";
 	}
 	
-	@RequestMapping(value="/users",method=RequestMethod.POST)
+	@RequestMapping(value="",method=RequestMethod.POST)
 	public String create(@Valid User user, BindingResult bindingResult) {
-		log.debug("User :{}" ,user);
+		log.debug("User :{}" ,user);		
 		if(bindingResult.hasErrors()) {
 			log.debug("Binding result has error!" );
 			List<ObjectError> errors = bindingResult.getAllErrors();
 			for (ObjectError error : errors) {
 				log.debug("error: {}",error.getDefaultMessage());
 			}
+			return "users/form";
 		}
-		return "users/form";
+		return "redirect:/";
+	}
+	
+	
+	@RequestMapping("/login/form")
+	public String loginForm(Model model) {
+		model.addAttribute("user", new User());
+		return "users/login";
+	}
+	
+	@RequestMapping("/login")
+	public String login(@Valid User user, BindingResult bindingResult, HttpSession session, Model model) {
+		log.debug("User :{}" ,user);
+		if(bindingResult.hasErrors()) {
+			return "users/login"; 
+		}
+		String userId = user.getUserId();
+		String userPw = user.getPassword();
+		if(userId == null) {
+			model.addAttribute("errorMessage", "존재하지 않는 사용자입니다.");
+			// 에러 처리 - 존재하지 않는 사용자입니다.
+			return "users/login";
+			
+		}if(!userPw.equals(user.getPassword())){
+			// 에러 처리 - 비밀번호가 틀립니다.
+			return "users/login";
+		}		
+		session.setAttribute("userId", user.getUserId());
+		// 세션에 사용자 정보 저장.
+		return "redirect:/";
 	}
 }
