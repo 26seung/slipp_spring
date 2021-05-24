1bfe158730087e958ee20e5c6adbba99bae2f157
26seung <yousi7@naver.com>
2021-05-24 23:56:46 +0900

    dao

--- slipp_spring/src/main/java/net/slipp/dao/users/UserDao.java
+++ slipp_spring/src/main/java/net/slipp/dao/users/UserDao.java
@@ -1,7 +1,20 @@
 package net.slipp.dao.users;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 public class UserDao extends JdbcDaoSupport {
-
+// 데이터베이스 초기화 사용위해 pom 에 jdbc설치
+	
+	@PostConstruct
+	public void initialize() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("slipp.sql"));
+		DatabasePopulatorUtils.execute(populator, getDataSource());
+	}
 }
