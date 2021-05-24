02a21dc1ae0fbfed6afab9da6ca844ecd05b1f9f
26seung <yousi7@naver.com>
2021-05-25 01:02:15 +0900

    DB initialize & UserDao 구현

--- slipp_spring/src/main/java/net/slipp/dao/users/UserDao.java
+++ slipp_spring/src/main/java/net/slipp/dao/users/UserDao.java
@@ -1,12 +1,17 @@
 package net.slipp.dao.users;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import net.slipp.domain.users.User;
 
 public class UserDao extends JdbcDaoSupport {
 // 데이터베이스 초기화 사용위해 pom 에 jdbc설치
@@ -16,5 +21,30 @@ public class UserDao extends JdbcDaoSupport {
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
 		populator.addScript(new ClassPathResource("slipp.sql"));
 		DatabasePopulatorUtils.execute(populator, getDataSource());
+	}
+	
+	public User findById(String userId) {		
+		String sql = "select * from USERS where userId = ?";
+		
+		RowMapper<User> rowMapper = new RowMapper<User>() {
+			
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				// TODO Auto-generated method stub
+				return new User(
+						rs.getString("userId"),
+						rs.getString("password"),
+						rs.getString("name"),
+						rs.getString("email")
+						);
+			}
+		};
+		return getJdbcTemplate().queryForObject(sql, rowMapper, userId);
+	}
+	
+	public void create(User user) {		
+		String sql = "insert into USERS values(?, ?, ?, ?)";
+		
+		getJdbcTemplate().update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
 	}
 }
