f5e7e4a0ce6ce722c61352954b75abfc38e6371c
26seung <yousi7@naver.com>
2021-05-27 23:20:05 +0900

    userDao.InterFace(컨트롤러와 dao 와의 결함을 낮춤)

--- slipp_spring/src/main/java/net/slipp/dao/users/UserDao.java
+++ slipp_spring/src/main/java/net/slipp/dao/users/UserDao.java
@@ -1,62 +1,13 @@
 package net.slipp.dao.users;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-
 import net.slipp.domain.users.User;
 
-public class UserDao extends JdbcDaoSupport {
-// 데이터베이스 초기화 사용위해 pom 에 jdbc설치
-	
-	private static final Logger log = LoggerFactory.getLogger(UserDao.class);
-	
-	@PostConstruct
-	public void initialize() {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("slipp.sql"));
-		DatabasePopulatorUtils.execute(populator, getDataSource());
-		log.info("database initialized success!");
-	}
-	
-	public User findById(String userId) {		
-		String sql = "select * from USERS where userId = ?";
-		
-		RowMapper<User> rowMapper = new RowMapper<User>() {
-			
-			@Override
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				// TODO Auto-generated method stub
-				return new User(
-						rs.getString("userId"),
-						rs.getString("password"),
-						rs.getString("name"),
-						rs.getString("email")
-						);
-			}
-		};
-		return getJdbcTemplate().queryForObject(sql, rowMapper, userId);
-	}
-	
-	public void create(User user) {		
-		String sql = "insert into USERS values(?, ?, ?, ?)";
-		
-		getJdbcTemplate().update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
-	}
+public interface UserDao {
 
-	public void update(User user) {
-		String sql = "update USERS set password = ?, name = ?, email = ? where userId = ?";
-		
-		getJdbcTemplate().update(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
-		
-	}
+	User findById(String userId);
+
+	void create(User user);
+
+	void update(User user);
+
 }
